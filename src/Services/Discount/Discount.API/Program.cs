90eed289cbe35c895d88122f189cc6dd568862d6
eren subaş <erensubas@gmail.com>
2023-05-11 12:06:22 +0300

    discout api hostservice, database migration added

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddHostedService<DatabaseMigrater>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
